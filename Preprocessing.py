8ea11f8ac37f9690c1bbd027339cead3c3bbe37b
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-06-02 11:14:43 +0530

    Update Preprocessing.py
    UPDATE - Made the autocorrelation function

--- Preprocessing.py
+++ Preprocessing.py
@@ -11,9 +11,27 @@ for i in range(15):
     df[str(i)] = pd.read_csv('data/19279_18.95_72.85_20{:02d}.csv'.format(i), 
                              header = 2)
 
-def autocorrelation(df):
-   
-    return
+def autocorrelation(data, k):
+    """
+    Parameters
+    ----------
+    data : ndarray
+        The input data from which autocorrelation needs to be calculated.
+    k : int
+        The offset of hours by which autocorrelation needs to be calculated.
+
+    Returns
+    -------
+    r : float
+        Returns the autocorrelation of the data.
+    """
+    var = np.var(data)
+    mean = np.mean(data)
+    N = len(data) # 8760 in this case
+    
+    r = np.sum((data[k:] - mean) * (data[:N-k] - mean)) / ((N - k)*var)
+    
+    return r
 
 def normalizeSigmoid():
     
