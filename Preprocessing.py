d20322863fae3be6dd91df3975e885b16b993e4a
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-06-02 10:29:26 +0530

    Update Preprocessing.py
    UPDATE - read data in the beginning

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 
-def autocorrelation():
-    
+import pandas as pd
+import numpy as np
+
+# initializing dictionary that will contain each year's data
+df = {}
+
+# reading all data into df
+for i in range(15):
+    df[str(i)] = pd.read_csv('data/19279_18.95_72.85_20{:02d}.csv'.format(i), 
+                             header = 2)
+
+def autocorrelation(df):
+   
     return
 
 def normalizeSigmoid():
