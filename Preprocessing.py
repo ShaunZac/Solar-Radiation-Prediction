7d6e1a9e939040ae34b50cc172aea140e2c5052d
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-06-03 21:35:29 +0530

    Update Preprocessing.py
    Added functions normalizeSigmoid and initializeInputOutput.

--- Preprocessing.py
+++ Preprocessing.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 # initializing dictionary that will contain each year's data
 df = {}
@@ -67,3 +68,49 @@ def getCorrelationTable(df):
     
     return corr_table
 
+def normalizeSigmoid(df):
+    
+    mu = np.mean(df,axis=0)
+    SD = np.std(df, axis = 0)
+    
+    return 1/(1 + np.exp(-(df-mu)/SD))
+
+def initializeInputOutput(df, predict_hour, save = True):
+    
+    # making the full dataframe
+    full_df = df['0'].copy()
+    for i in range(1, 15):
+        full_df = full_df.append(df[str(i)].copy())
+    full_df = full_df.reset_index(drop = True)
+    
+    # dropping unnecessary columns
+    full_df = full_df.drop(['DNI', 'GHI', 'Clearsky DHI', 'Clearsky DNI', 
+                            'Clearsky GHI', 'Snow Depth', 'Fill Flag'], axis = 1)
+    # dropping the first few rows since prediction requires
+    # 'predict_hour' number of prev data
+    y = full_df['DHI'].iloc[predict_hour:].copy()
+    X = full_df.copy()
+    X['DHI'] = X['DHI'].shift(predict_hour)
+    X = X.dropna()
+    
+    # split into train test samples
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/15,
+                                                        random_state = 0)
+    
+    # drop all training examples before 5 am and after 8 pm
+    y_train = y_train.drop(index = X_train[(X_train['Hour'] >= 20) | (X_train['Hour'] <= 5)].index)
+    X_train = X_train[(X_train['Hour'] < 20) & (X_train['Hour'] > 5)]
+    
+    # normalizing the train and test inputs
+    X_train = normalizeSigmoid(X_train)
+    X_test = normalizeSigmoid(X_test)
+    
+    if save:
+        X_train.to_csv('IO/X_train.csv')
+        y_train.to_csv('IO/y_train.csv')
+        X_test.to_csv('IO/X_test.csv')
+        y_test.to_csv('IO/y_test.csv')
+    
+    return X_train, X_test, y_train, y_test
+
+X_train, X_test, y_train, y_test = initializeInputOutput(df, 1)
